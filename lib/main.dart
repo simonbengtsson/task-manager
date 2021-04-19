cbd202d45f420c1020397195a58cd8a3bed95204
Simon Bengtsson <simonbengtsson@users.noreply.github.com>
2021-04-20 00:13:32 +0200

    Drag and drop v1

--- lib/main.dart
+++ lib/main.dart
@@ -105,9 +105,9 @@ class _MainScreenState extends State<MainScreen> {
   }
 
   Widget buildTaskList(BuildContext context) {
-    return ReorderableListView(
+    return ListView(
       padding: EdgeInsets.symmetric(vertical: 20),
-      onReorder: (int oldIndex, int newIndex) {
+      /*onReorder: (int oldIndex, int newIndex) {
         setState(() {
           if (oldIndex < newIndex) {
             newIndex -= 1;
@@ -115,7 +115,7 @@ class _MainScreenState extends State<MainScreen> {
           var item = todayTasks.removeAt(oldIndex);
           todayTasks.insert(newIndex, item);
         });
-      },
+      },*/
       children: [
         for (var entry in todayTasks.asMap().entries)
           buildTaskRow(context, entry.key, entry.value)
@@ -150,44 +150,55 @@ class _MainScreenState extends State<MainScreen> {
   }
 
   Widget buildCalendarDay(Day day) {
-    return Container(
-      width: 200,
-      decoration: BoxDecoration(
-        color: Colors.grey[[6, 7].contains(day.date.weekday) ? 300 : 200],
-        border: Border.all(color: Colors.grey[100]!),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 10),
-              child: Text(
-                "${weekdays[day.date.weekday - 1]} ${day.date.day}",
-                style: TextStyle(
-                    fontWeight: FontWeight.w500, color: Colors.grey[500]),
-              ),
-            ),
-            buildCalendarDayTaskList(day),
-          ],
+    return DragTarget<Task>(onAccept: (task) {
+      setState(() {
+        day.tasks.add(task);
+      });
+    }, builder: (context, candidateItems, rejectedItems) {
+      return Container(
+        width: 200,
+        decoration: BoxDecoration(
+          color: Colors.grey[[6, 7].contains(day.date.weekday) ? 300 : 200],
+          border: Border.all(color: Colors.grey[100]!),
         ),
-      ),
-    );
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "${weekdays[day.date.weekday - 1]} ${day.date.day}",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500, color: Colors.grey[500]),
+                ),
+              ),
+              buildCalendarDayTaskList(day),
+            ],
+          ),
+        ),
+      );
+    });
   }
 
   Widget buildCalendarDayTaskList(Day day) {
     return Column(
       children: [
-        for (var task in day.tasks) Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 5.0),
-              child: Icon(Icons.radio_button_unchecked, size: 14, color: Colors.grey[600],),
-            ),
-            Text(task.text),
-          ],
-        ),
+        for (var task in day.tasks)
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 5.0),
+                child: Icon(
+                  Icons.radio_button_unchecked,
+                  size: 14,
+                  color: Colors.grey[600],
+                ),
+              ),
+              Text(task.text),
+            ],
+          ),
       ],
     );
   }
@@ -235,49 +246,80 @@ class _MainScreenState extends State<MainScreen> {
             ),
           );
 
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 10),
-      decoration: BoxDecoration(
-        color: Colors.grey[200],
-        border: Border.all(color: Colors.grey[100]!),
-      ),
-      key: Key(index.toString()),
-      height: 50,
-      child: Row(children: [
-        Padding(
-            padding: EdgeInsets.only(right: 10),
-            child: IconButton(
-              hoverColor: Colors.transparent,
-              splashColor: Colors.transparent,
-              highlightColor: Colors.transparent,
-              icon: Icon(
-                  task.completedAt == null
-                      ? Icons.radio_button_unchecked
-                      : Icons.check_circle,
-                  color: Colors.grey[400]),
-              onPressed: () {
-                setState(() {
-                  var doneIndex =
-                      todayTasks.indexWhere((element) => element.done);
-                  if (task.done) {
-                    var firstCompletedIndex =
-                        doneIndex < 0 ? todayTasks.length - 1 : doneIndex;
-                    task.completedAt = null;
-                    var index = todayTasks.indexOf(task);
-                    var item = todayTasks.removeAt(index);
-                    todayTasks.insert(firstCompletedIndex, item);
-                  } else {
-                    var firstCompletedIndex =
-                        doneIndex < 0 ? todayTasks.length - 1 : doneIndex - 1;
-                    task.completedAt = DateTime.now();
-                    todayTasks.remove(task);
-                    todayTasks.insert(firstCompletedIndex, task);
-                  }
-                });
-              },
-            )),
-        textField,
+    return LongPressDraggable(
+      data: task,
+      dragAnchorStrategy: pointerDragAnchorStrategy,
+      feedback: Row(children: [
+        ConstrainedBox(
+          constraints: BoxConstraints(minWidth: 300),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  blurRadius: 7,
+                ),
+              ],
+            ),
+            padding: EdgeInsets.all(10),
+            child: Text(task.text,
+                style: TextStyle(
+                  fontSize: 15,
+                    color: task.completedAt == null ? Colors.black : Colors.grey,
+                    decoration: task.completedAt == null
+                        ? TextDecoration.none
+                        : TextDecoration.lineThrough)),
+          ),
+        ),
       ]),
+      onDragCompleted: () {
+        todayTasks.remove(task);
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          border: Border.all(color: Colors.grey[100]!),
+        ),
+        key: Key(index.toString()),
+        height: 50,
+        child: Row(children: [
+          Padding(
+              padding: EdgeInsets.only(right: 10),
+              child: IconButton(
+                hoverColor: Colors.transparent,
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                icon: Icon(
+                    task.completedAt == null
+                        ? Icons.radio_button_unchecked
+                        : Icons.check_circle,
+                    color: Colors.grey[400]),
+                onPressed: () {
+                  setState(() {
+                    var doneIndex =
+                        todayTasks.indexWhere((element) => element.done);
+                    if (task.done) {
+                      var firstCompletedIndex =
+                          doneIndex < 0 ? todayTasks.length - 1 : doneIndex;
+                      task.completedAt = null;
+                      var index = todayTasks.indexOf(task);
+                      var item = todayTasks.removeAt(index);
+                      todayTasks.insert(firstCompletedIndex, item);
+                    } else {
+                      var firstCompletedIndex =
+                          doneIndex < 0 ? todayTasks.length - 1 : doneIndex - 1;
+                      task.completedAt = DateTime.now();
+                      todayTasks.remove(task);
+                      todayTasks.insert(firstCompletedIndex, task);
+                    }
+                  });
+                },
+              )),
+          textField,
+        ]),
+      ),
     );
   }
 }
