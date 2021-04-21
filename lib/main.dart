dac3d7e5aa7c08e038bb8f1a3cd9e03cadd4d33d
Simon Bengtsson <simonbengtsson@users.noreply.github.com>
2021-04-21 22:46:52 +0200

    Add calendar sync

--- lib/main.dart
+++ lib/main.dart
@@ -26,14 +26,12 @@ class _MainScreenState extends State<MainScreen> {
   List<Task> todayTasks = [];
   List<Day> days = [];
   Task? activeTask;
-  bool demoMode = false;
 
   @override
   initState() {
     var now = DateTime.now();
     days = List<Day>.generate(14, (int i) {
-      var timestamp = now.millisecondsSinceEpoch +
-          (i + 1) * 24 * 3600 * 1000;
+      var timestamp = now.millisecondsSinceEpoch + (i + 1) * 24 * 3600 * 1000;
       var date = DateTime.fromMillisecondsSinceEpoch(timestamp);
       var day = Day(date);
       return day;
@@ -41,19 +39,35 @@ class _MainScreenState extends State<MainScreen> {
 
     ValueStore().loadTasks().then((tasks) {
       setState(() {
-        todayTasks = tasks;
+        todayTasks.addAll(tasks);
       });
     });
 
+    loadCalendarEvents();
+
     super.initState();
+  }
+
+  loadCalendarEvents() async {
+    var tasks = await ValueStore().loadCalendarEvents();
+    tasks.forEach((element) {
+      print(element.text +
+          ' ' +
+          element.date!.toIso8601String() +
+          ' ' +
+          (element.calendarId ?? ''));
+    });
+    print("Fetched ${tasks.length}");
+    setState(() {
+      todayTasks.removeWhere((element) => element.fromCalendar);
+      todayTasks.addAll(tasks);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     // Find a better place for this?
-    if (!demoMode) {
-      ValueStore().saveTasks(todayTasks);
-    }
+    ValueStore().saveTasks(todayTasks);
 
     return Scaffold(
       body: Column(
@@ -75,6 +89,22 @@ class _MainScreenState extends State<MainScreen> {
         ],
       ),
     );
+  }
+
+  loadDemoTasks() {
+    todayTasks.add(Task('Buy milk'));
+    todayTasks.add(Task('Run 5km'));
+    todayTasks.add(Task('Walk the dog'));
+
+    var food = Task('Food Conference');
+    food.date = DateTime.fromMillisecondsSinceEpoch(
+        DateTime.now().millisecondsSinceEpoch - 48 * 3600 * 1000);
+    todayTasks.add(food);
+
+    var yoga = Task('Yoga');
+    yoga.date = DateTime.fromMillisecondsSinceEpoch(
+        DateTime.now().millisecondsSinceEpoch + 24 * 3600 * 1000);
+    todayTasks.add(yoga);
   }
 
   Widget buildHeader(BuildContext context) {
@@ -107,40 +137,79 @@ class _MainScreenState extends State<MainScreen> {
           PopupMenuButton(
             icon: Icon(Icons.more_horiz),
             onSelected: (value) async {
-              var loadedTasks = await ValueStore().loadTasks();
               if (value == "demo") {
                 setState(() {
-                  demoMode = !demoMode;
-                  if (demoMode) {
-                    todayTasks = [];
-
-                    todayTasks.add(Task('Buy milk'));
-                    todayTasks.add(Task('Run 5km'));
-                    todayTasks.add(Task('Walk the dog'));
-
-                    var food = Task('Food Conference');
-                    food.date = DateTime.fromMillisecondsSinceEpoch(DateTime.now().millisecondsSinceEpoch - 48 * 3600 * 1000);
-                    todayTasks.add(food);
-
-                    var yoga = Task('Yoga');
-                    yoga.date = DateTime.fromMillisecondsSinceEpoch(DateTime.now().millisecondsSinceEpoch + 24 * 3600 * 1000);
-                    todayTasks.add(yoga);
-                  } else {
-                    todayTasks = loadedTasks;
-                  }
+                  loadDemoTasks();
                 });
+              } else if (value == "removeAllTasks") {
+                setState(() {
+                  todayTasks = [];
+                });
+              } else if (value == "addCalendar") {
+                presentAddCalendarDialog(context);
+              } else if (value == "removeAllCalendars") {
+                await ValueStore().saveCalendars([]);
+                loadCalendarEvents();
               }
             },
             itemBuilder: (BuildContext context) {
-              var item = PopupMenuItem<String>(
+              var demo = PopupMenuItem<String>(
                 value: "demo",
-                child: Text(demoMode ? 'Turn off Demo' : "Switch to Demo"),
+                child: Text('Load Demo'),
               );
-              return [item];
+              var rmTasks = PopupMenuItem<String>(
+                value: "removeAllTasks",
+                child: Text('Remove all Tasks'),
+              );
+              var rmCalendars = PopupMenuItem<String>(
+                value: "removeAllCalendars",
+                child: Text('Remove all Calendars'),
+              );
+              var addCalendar = PopupMenuItem<String>(
+                value: "addCalendar",
+                child: Text('Add Calendar'),
+              );
+              return [addCalendar, demo, rmTasks, rmCalendars];
             },
           ),
         ],
       ),
+    );
+  }
+
+  TextEditingController _textFieldController = TextEditingController();
+
+  presentAddCalendarDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('TextField in Dialog'),
+          content: TextField(
+            controller: _textFieldController,
+            decoration: InputDecoration(hintText: "Text Field in Dialog"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              child: Text('Add'),
+              onPressed: () async {
+                Navigator.pop(context);
+                print(_textFieldController.text);
+                var calendars = await ValueStore().getCalendars();
+                calendars.add(_textFieldController.text);
+                await ValueStore().saveCalendars(calendars);
+                await loadCalendarEvents();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 
@@ -162,11 +231,16 @@ class _MainScreenState extends State<MainScreen> {
           });
         },*/
         children: [
-          for (var entry in todayTasks.where((element) {
-            var date = element.date;
-            if (date == null) return true;
-            return Day(date).daySince1970 <= Day(DateTime.now()).daySince1970;
-          }).toList().asMap().entries)
+          for (var entry in todayTasks
+              .where((element) {
+                var date = element.date;
+                if (date == null) return true;
+                return Day(date).daySince1970 <=
+                    Day(DateTime.now()).daySince1970;
+              })
+              .toList()
+              .asMap()
+              .entries)
             buildTaskRow(context, entry.key, entry.value)
         ],
       );
@@ -223,58 +297,156 @@ class _MainScreenState extends State<MainScreen> {
     });
     return Column(
       children: [
-        for (var task in dayTasks)
-          LongPressDraggable(
-            feedback: Row(children: [
-              ConstrainedBox(
-                constraints: BoxConstraints(minWidth: 300),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.3),
-                        blurRadius: 7,
-                      ),
-                    ],
-                  ),
-                  padding: EdgeInsets.all(10),
-                  child: Text(task.text,
-                      style: TextStyle(
-                          fontSize: 15,
-                          color:
-                          task.completedAt == null ? Colors.black : Colors.grey,
-                          decoration: task.completedAt == null
-                              ? TextDecoration.none
-                              : TextDecoration.lineThrough)),
-                ),
-              ),
-            ]),
-            data: task,
-            dragAnchorStrategy: pointerDragAnchorStrategy,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(right: 5.0),
-                    child: Icon(
-                      Icons.radio_button_unchecked,
-                      size: 14,
-                      color: Colors.grey[600],
-                    ),
-                  ),
-                  Expanded(child: Text(task.text)),
-                ],
-              ),
-            ),
-          ),
+        for (var task in dayTasks) buildFutureTaskRow(task),
       ],
     );
   }
 
+  Widget buildFutureTaskRow(Task task) {
+    if (task.fromCalendar || task.done) {
+      return buildFutureTaskInsideRow(task);
+    }
+    return LongPressDraggable(
+      feedback: Row(children: [
+        ConstrainedBox(
+          constraints: BoxConstraints(minWidth: 300),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  blurRadius: 7,
+                ),
+              ],
+            ),
+            padding: EdgeInsets.all(10),
+            child: Text(task.text,
+                style: TextStyle(
+                    fontSize: 15,
+                    color:
+                        task.completedAt == null ? Colors.black : Colors.grey,
+                    decoration: task.completedAt == null
+                        ? TextDecoration.none
+                        : TextDecoration.lineThrough)),
+          ),
+        ),
+      ]),
+      data: task,
+      dragAnchorStrategy: pointerDragAnchorStrategy,
+      child: buildFutureTaskInsideRow(task),
+    );
+  }
+
+  Widget buildFutureTaskInsideRow(Task task) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 5.0),
+            child: Icon(
+              task.fromCalendar
+                  ? Icons.calendar_today_rounded
+                  : Icons.radio_button_unchecked,
+              size: 14,
+              color: Colors.grey[600],
+            ),
+          ),
+          Expanded(child: Text(task.text)),
+        ],
+      ),
+    );
+  }
+
   Widget buildTaskRow(BuildContext context, int index, Task task) {
-    var textField = task == activeTask
+    if (task.fromCalendar || task.done) {
+      return buildTaskInsideRow(task, index);
+    }
+    return LongPressDraggable(
+      data: task,
+      dragAnchorStrategy: pointerDragAnchorStrategy,
+      feedback: Row(children: [
+        ConstrainedBox(
+          constraints: BoxConstraints(minWidth: 300),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  blurRadius: 7,
+                ),
+              ],
+            ),
+            padding: EdgeInsets.all(10),
+            child: Text(task.text,
+                style: TextStyle(
+                    fontSize: 15,
+                    color:
+                        task.completedAt == null ? Colors.black : Colors.grey,
+                    decoration: task.completedAt == null
+                        ? TextDecoration.none
+                        : TextDecoration.lineThrough)),
+          ),
+        ),
+      ]),
+      child: buildTaskInsideRow(task, index),
+    );
+  }
+
+  Widget buildTaskInsideRow(Task task, int index) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        border: Border.all(color: Colors.grey[100]!),
+      ),
+      key: Key(index.toString()),
+      height: 50,
+      child: Row(children: [
+        Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: IconButton(
+              hoverColor: Colors.transparent,
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              icon: Icon(
+                  task.fromCalendar
+                      ? Icons.calendar_today_rounded
+                      : (task.completedAt == null
+                          ? Icons.radio_button_unchecked
+                          : Icons.check_circle),
+                  color: Colors.grey[400]),
+              onPressed: () {
+                if (task.fromCalendar) return;
+                setState(() {
+                  var doneIndex =
+                      todayTasks.indexWhere((element) => element.done);
+                  if (task.done) {
+                    var firstCompletedIndex =
+                        doneIndex < 0 ? todayTasks.length - 1 : doneIndex;
+                    task.completedAt = null;
+                    var index = todayTasks.indexOf(task);
+                    var item = todayTasks.removeAt(index);
+                    todayTasks.insert(firstCompletedIndex, item);
+                  } else {
+                    var firstCompletedIndex =
+                        doneIndex < 0 ? todayTasks.length - 1 : doneIndex - 1;
+                    task.completedAt = DateTime.now();
+                    todayTasks.remove(task);
+                    todayTasks.insert(firstCompletedIndex, task);
+                  }
+                });
+              },
+            )),
+        buildTaskRowText(task),
+      ]),
+    );
+  }
+
+  Widget buildTaskRowText(Task task) {
+    return task == activeTask
         ? Expanded(
             child: Padding(
               padding: EdgeInsets.only(right: 30),
@@ -285,7 +457,7 @@ class _MainScreenState extends State<MainScreen> {
                   setState(() {
                     activeTask = null;
                     if (task.text.isEmpty) {
-                      todayTasks.removeAt(index);
+                      todayTasks.remove(task);
                     }
                   });
                 },
@@ -315,79 +487,5 @@ class _MainScreenState extends State<MainScreen> {
                           : TextDecoration.lineThrough)),
             ),
           );
-
-    return LongPressDraggable(
-      data: task,
-      dragAnchorStrategy: pointerDragAnchorStrategy,
-      feedback: Row(children: [
-        ConstrainedBox(
-          constraints: BoxConstraints(minWidth: 300),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.grey[200],
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.3),
-                  blurRadius: 7,
-                ),
-              ],
-            ),
-            padding: EdgeInsets.all(10),
-            child: Text(task.text,
-                style: TextStyle(
-                    fontSize: 15,
-                    color:
-                        task.completedAt == null ? Colors.black : Colors.grey,
-                    decoration: task.completedAt == null
-                        ? TextDecoration.none
-                        : TextDecoration.lineThrough)),
-          ),
-        ),
-      ]),
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        decoration: BoxDecoration(
-          color: Colors.grey[200],
-          border: Border.all(color: Colors.grey[100]!),
-        ),
-        key: Key(index.toString()),
-        height: 50,
-        child: Row(children: [
-          Padding(
-              padding: EdgeInsets.only(right: 10),
-              child: IconButton(
-                hoverColor: Colors.transparent,
-                splashColor: Colors.transparent,
-                highlightColor: Colors.transparent,
-                icon: Icon(
-                    task.completedAt == null
-                        ? Icons.radio_button_unchecked
-                        : Icons.check_circle,
-                    color: Colors.grey[400]),
-                onPressed: () {
-                  setState(() {
-                    var doneIndex =
-                        todayTasks.indexWhere((element) => element.done);
-                    if (task.done) {
-                      var firstCompletedIndex =
-                          doneIndex < 0 ? todayTasks.length - 1 : doneIndex;
-                      task.completedAt = null;
-                      var index = todayTasks.indexOf(task);
-                      var item = todayTasks.removeAt(index);
-                      todayTasks.insert(firstCompletedIndex, item);
-                    } else {
-                      var firstCompletedIndex =
-                          doneIndex < 0 ? todayTasks.length - 1 : doneIndex - 1;
-                      task.completedAt = DateTime.now();
-                      todayTasks.remove(task);
-                      todayTasks.insert(firstCompletedIndex, task);
-                    }
-                  });
-                },
-              )),
-          textField,
-        ]),
-      ),
-    );
   }
 }
--- lib/models.dart
+++ lib/models.dart
@@ -1,7 +1,42 @@
 import 'dart:convert';
+import 'dart:math';
+
+import 'package:http/http.dart' as http;
+import 'package:icalendar_parser/icalendar_parser.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ValueStore {
+  saveCalendars(List<String> calendarUrls) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String jsonString = jsonEncode(calendarUrls.toSet().toList());
+    await prefs.setString('calendars', jsonString);
+  }
+
+  Future<List<String>> getCalendars() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String jsonString = prefs.getString('calendars') ?? '[]';
+    List<dynamic> saved = jsonDecode(jsonString);
+    return saved.map((e) => e as String).toList();
+  }
+
+  Future<List<Task>> loadCalendarEvents() async {
+    List<String> saved = await getCalendars();
+
+    List<Task> allTasks = [];
+    for (var calendarUrl in saved) {
+      var res = await http.get(Uri.parse(calendarUrl));
+      var lines = res.body.split('\r\n');
+      print("Lines: ${lines.length}");
+      final calendar = ICalendar.fromLines(lines);
+      final events =
+        calendar.data!.where((element) => element['type'] == 'VEVENT' && Day(element['dtstart'] as DateTime).daySince1970 >= Day(DateTime.now()).daySince1970).toList();
+      print(events[0]);
+      var tasks = events.map((e) => Task.fromIcs(e)).toList();
+      allTasks.addAll(tasks);
+    }
+    return allTasks;
+  }
+
   Future<List<Task>> loadTasks() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String jsonString = prefs.getString('tasks') ?? '[]';
@@ -14,18 +49,33 @@ class ValueStore {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     var data = tasks.map((e) => e.toJson()).toList();
     String jsonString = jsonEncode(data);
-    prefs.setString('tasks', jsonString);
-    return tasks;
+    await prefs.setString('tasks', jsonString);
   }
+}
+
+Random random = Random();
+
+String generateId() {
+  const chars =
+      'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+  int length = 10;
+  var item = Iterable.generate(
+      length, (_) => chars.codeUnitAt(random.nextInt(chars.length)));
+  return String.fromCharCodes(item);
 }
 
 class Task {
   String text;
+  String? calendarId;
   DateTime? completedAt;
   DateTime? date;
 
   bool get done {
     return completedAt != null;
+  }
+
+  bool get fromCalendar {
+    return calendarId != null;
   }
 
   Task(this.text);
@@ -35,22 +85,35 @@ class Task {
   }
 
   Map<String, dynamic> toJson() {
-    var map = { 'text': text };
+    var map = {'text': text};
     if (completedAt != null)
       map['completedAt'] = completedAt!.toIso8601String();
-    if (date != null)
-      map['date'] = date!.toIso8601String();
+    if (date != null) map['date'] = date!.toIso8601String();
+    if (calendarId != null) map['calendarId'] = calendarId!;
     return map;
+  }
+
+  factory Task.fromIcs(Map<String, dynamic> ics) {
+    var calendarId = ics["uid"] as String;
+    var text = ics["summary"] as String;
+    var startsAt = ics["dtstart"] as DateTime;
+
+    var task = Task(text);
+    task.calendarId = calendarId;
+    task.date = startsAt;
+
+    return task;
   }
 
   factory Task.fromJson(Map<String, dynamic> json) {
     var text = json["text"] as String;
+    var calendarId = json["calendarId"] as String?;
     var dateString = json["date"] as String?;
     var completedAtString = json["completedAt"] as String?;
 
     var task = Task(text);
-    if (dateString != null)
-      task.date = DateTime.parse(dateString);
+    if (calendarId != null) task.calendarId = calendarId;
+    if (dateString != null) task.date = DateTime.parse(dateString);
     if (completedAtString != null)
       task.completedAt = DateTime.parse(completedAtString);
 
@@ -69,4 +132,17 @@ class Day {
 }
 
 var weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+var months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
+];
